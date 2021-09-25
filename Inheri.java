b128f01c6a5b77950b67e294c2551774af28205a
Evani Venkata Sai Sarvan Kumar Sarma <48446039+evanisaisarvan1213@users.noreply.github.com>
2021-09-25 09:30:20 +0530

    Add files via upload

---
+++ Inheri.java
@@ -0,0 +1,24 @@
+class A
+{
+   public A()
+   {
+       System.out.println("A Cons");
+   }
+ }
+class B extends A
+{
+    public B()
+   {
+       System.out.println("B Cons");
+   }
+
+}
+class Inheri
+{
+    public static void main(String[] args)
+    {
+        B b1=new B();
+        
+        
+    }
+}
