63fa8ac1f47d546c880084af615da489669bbd71
evssksarma@gmail.com <evssksarma@gmail.com>
2021-10-04 15:53:51 +0530

    Mobiles.java

---
+++ Mobiles.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+import java.util.ArrayList;
+class Mobiles
+{
+    HashMap<String,ArrayList<String>> mobiles = new HashMap<>();
+    Mobiles() {}
+    public ArrayList<String> getModels(String company)
+    {
+         return null;
+    }
+
+}
